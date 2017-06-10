fc0a9a6b6eb927a3e6ae2f39809565f1442570cd
Renan PORCON <rporcon@student.42.fr>
2017-06-10 18:18:15 +0200

    save

---
+++ Colleen.c
@@ -0,0 +1,20 @@
+
+/*
+**	func
+*/
+int	func(int num)
+{
+	return ((num = 42))
+}
+
+int		main(void)
+{
+	/*
+	**	main
+	*/
+	int		a;
+
+	a = func(a);
+	char	*str = "monprog";
+	return (0);
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+
+
+int		main(void)
+{
+	char	*str;
+
+	str = "str = ";
+	return (0);
+}
