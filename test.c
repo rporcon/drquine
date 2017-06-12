a2edc4b74f1616410b9b43a7a8212513d8eca3fc
Renan PORCON <rporcon@student.42.fr>
2017-06-12 19:07:40 +0200

    bug with test.c

--- test.c
+++ test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int		main(void)
 {
-	printf("%cc", 42, 37);
+	char	filename[42];
+	char	command[42];
+
+	char	*prog = "TEST%c";
+	sprintf(filename, "Test_%d.c", 5);
+	strcpy(command, "env -i SULLY=1 gcc ");
+	fprintf(fopen(filename, "w+"), prog, 42);
+	system(strcat(command, filename));
 	return (0);
 }
