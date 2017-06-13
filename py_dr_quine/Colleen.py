700229d0dd731c0c8604f2b854fc33050bb0b07e
Renan PORCON <rporcon@student.42.fr>
2017-06-13 12:32:10 +0200

    save

---
+++ py_dr_quine/Colleen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    func
+"""
+def print_array(i):
+    return
+def main():
+    """
+    	main
+    """
+    prog = "#!/usr/bin/python3{}{}{}{}{}    func{}{}{}{}{}def   print_array(i):{}   return{}def main():{}   {}{}{}{}\t\tmain{}\t{}{}{}{}\tprog = {}{}if __name__ == {}__main__{}:{}\tmain();";
+    print(prog.format(chr(10), chr(34), chr(34), chr(34), chr(10), chr(10), chr(34), chr(34), chr(34), chr(10), chr(10), chr(10), chr(10), chr(34), chr(34), chr(34), chr(10), chr(10), chr(34), chr(34), chr(34), chr(10), prog, chr(10), chr(34), chr(34), chr(10)));
+if __name__ == "__main__":
+    main();
