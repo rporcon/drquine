f5e661c84057366ca2d3cf07a63a7d66c52f3bae
Renan PORCON <rporcon@student.42.fr>
2017-06-13 17:37:55 +0200

    python sully ok

--- py_dr_quine/Sully.py
+++ py_dr_quine/Sully.py
@@ -7,7 +7,8 @@ def main():
     if os.getenv('SULLY_PY') == '1':
         i = i - 1;
     f = open('Sully_' + str(i) + '.py', 'w+');
-    prog = '#!/usr/bin/python3\nimport os\ndef main():\n\ti = {};\n\tif i <= 0:\n\t\treturn ;\n\tif os.getenv({}SULLY_PY{}) == {}1{}:\n\t\ti = i - 1;\n\tf = open({}Sully_{} + str(i) + {}.py{}, {}w+{});\n\tprog = {};\n\tf.write(prog.format(i, chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), repr(prog), chr(39), chr(39), chr(39), chr(39), chr(10)).expandtabs(4));\n\tos.system({}export SULLY_PY=1 && /usr/bin/python3 Sully_{} + str(i) + {}.py{});\nmain();{}';
+    prog = '#!/usr/bin/python3\nimport os\ndef main():\n\ti = {};\n\tif i <= 0:\n\t\treturn ;\n\tif os.getenv({}SULLY_PY{}) == {}1{}:\n\t\ti = i - 1;\n\tf = open({}Sully_{} + str(i) + {}.py{}, {}w+{});\n\tprog = {};\n\tf.write(prog.format(i, chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), repr(prog), chr(39), chr(39), chr(39), chr(39), chr(10)).expandtabs(4));\n\tf.close();\n\tos.system({}export SULLY_PY=1 && /usr/bin/python3 Sully_{} + str(i) + {}.py{});\nmain();{}';
     f.write(prog.format(i, chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), repr(prog), chr(39), chr(39), chr(39), chr(39), chr(10)).expandtabs(4));
+    f.close();
     os.system('export SULLY_PY=1 && /usr/bin/python3 Sully_' + str(i) + '.py');
 main();
--- py_dr_quine/test.py
+++
@@ -1,6 +0,0 @@
-def main():
-    a = 'a = %r';
-    print(a % a);
-
-if __name__ == "__main__":
-    main();
