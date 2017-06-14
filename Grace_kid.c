e59e6cb6e73b8974dfa0435e5e122876b8705a3c
Renan PORCON <rporcon@student.42.fr>
2017-06-14 12:57:52 +0200

    Grace ok

--- Grace_kid.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <fcntl.h>
-#define HELLO WORLD
-#define WORLD HELLO
-#define FT(x) int main(){char *a = "#include <stdio.h>%c#include <fcntl.h>%c#define HELLO WORLD%c#define WORLD HELLO%c#define FT(x) int main(){char *a = %c%s%c;fprintf(fopen(%cGrace_kid.c%c, %cw+%c),a,10,10,10,10,34,a,34,34,34,34,34,10,10);/* code */}%cFT(x);%c";fprintf(fopen("Grace_kid.c", "w+"),a,10,10,10,10,34,a,34,34,34,34,34,10,10);/* code */}
-FT(x);
