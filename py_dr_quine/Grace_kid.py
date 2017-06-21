fa79b55607e26ccda5c2dc86899fdd74e9e534a1
Renan PORCON <rporcon@student.42.fr>
2017-06-21 12:18:22 +0200

    delete Grace_kid.py

--- py_dr_quine/Grace_kid.py
+++
@@ -1,9 +0,0 @@
-DEFINE_1 = 1
-DEFINE_2 = 1
-
-def FT(x):
-    ''' code '''
-    prog = 'DEFINE_1 = 1\nDEFINE_2 = 1\n\ndef FT(x):\n\t{}{}{} code {}{}{}\n\tprog = {};\n\tf = open({}Grace_kid.py{}, {}w+{});\n\tf.write(prog.format(chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), repr(prog), chr(39), chr(39), chr(39), chr(39)).expandtabs(4));\nFT(42);\n';
-    f = open('Grace_kid.py', 'w+');
-    f.write(prog.format(chr(39), chr(39), chr(39), chr(39), chr(39), chr(39), repr(prog), chr(39), chr(39), chr(39), chr(39)).expandtabs(4));
-FT(42);
