7cfb1cb100ad0e5b7fdef615d80dd834e348eed6
Renan PORCON <rporcon@student.42.fr>
2017-06-21 12:08:24 +0200

    fix Grace.s

--- srcs/Grace.c
+++ srcs/Grace.c
@@ -2,5 +2,8 @@
 #include <fcntl.h>
 #define HELLO WORLD
 #define WORLD HELLO
-#define FT(x) int main(){char *a = "#include <stdio.h>%c#include <fcntl.h>%c#define HELLO WORLD%c#define WORLD HELLO%c#define FT(x) int main(){char *a = %c%s%c;fprintf(fopen(%c./srcs/Grace_kid.c%c, %cw+%c),a,10,10,10,10,34,a,34,34,34,34,34,10,10);/* code */}%cFT(x);%c";fprintf(fopen("./srcs/Grace_kid.c", "w+"),a,10,10,10,10,34,a,34,34,34,34,34,10,10);/* code */}
+/*
+	comm
+*/
+#define FT(x) int main(){char *a = "#include <stdio.h>%c#include <fcntl.h>%c#define HELLO WORLD%c#define WORLD HELLO%c/*%c%ccomm%c*/%c#define FT(x) int main(){char *a = %c%s%c;fprintf(fopen(%c./srcs/Grace_kid.c%c, %cw+%c),a,10,10,10,10,10,9,10,10,34,a,34,34,34,34,34,10,10);}%cFT(x);%c";fprintf(fopen("./srcs/Grace_kid.c", "w+"),a,10,10,10,10,10,9,10,10,34,a,34,34,34,34,34,10,10);}
 FT(x);
